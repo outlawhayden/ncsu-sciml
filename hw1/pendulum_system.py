79f831a302ca255aab8f1135b81956128a2009a8
hayden outlaw <haydenkoutlaw@gmail.com>
2025-09-02 11:00:24 -0400

    initialize repo

---
+++ hw1/pendulum_system.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# define constants
+g = 1
+l = 1
+m = 1
+
+# define initial condition
+x0 = np.array([[0],[1]], np.float64)
+
+
